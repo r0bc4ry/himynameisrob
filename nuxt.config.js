1638ad29dd8be56451ffb41ad23325fac9a99085
Rob Cary <r0bc4ry@gmail.com>
2019-08-13 15:10:59 -0400

    Fix lint again and add page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || 'Rob Cary',
+    title: 'Rob Cary',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
